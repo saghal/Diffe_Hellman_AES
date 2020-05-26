113c3c886eea7216bf2997767d4ec85caf0e90b9
saghal <mohammadsgh98@gmail.com>
2020-05-26 18:05:53 +0430

    edit initial parametrs message in client

--- client.py
+++ client.py
@@ -28,7 +28,7 @@ def decrypt_AES_GCM(message, Bnonc, BauthTag, secretKey):
     plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
     return plaintext
 
-cilent_private = 0x7250f5b473a13f2faffa851c4076bc2c # client private key
+cilentPrivateKey = 0x7250f5b473a13f2faffa851c4076bc2c # client private key
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -36,23 +36,23 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        data = s.recv(1024)
-        if not data:
+        b = b''
+        tmp = s.recv(1024)
+        if not tmp:
             continue
-        data = data.decode()
-        print('Received -------> "', data, '"')
-
-        if data.startswith('INITIAL_PARAMS'):                   # Receiving initial parameters
-            prime = int(re.findall('prime:([0-9]+),', data)[0])
-            g = int(re.findall('g:([0-9]+)', data)[0])
-
-            print('prime:' , hex(prime))
-            print('g:' , hex(g))
-
-            client_public = (g ^ cilent_private) % prime        # calculate public key
-            data = 'CLIENT_PUBLIC_KEY =' + str(client_public)
-            print("\nsending the client's public key to server...\n") # send client's public key without encryption
-            s.sendall(data.encode())
+        b = b + tmp
+        d = json.loads(b.decode('utf-8'))
+        if(d['set']=='initialParametrs'):
+            print('server registerd initial Parametrs')
+            prime = int(d['firstParam'])
+            print('prime: ',hex(prime))
+            print('generator: ',d['secondParam'])
+            clientPublicKey = (int(d['secondParam']) ^ cilentPrivateKey)% prime
+            print('client public key: ',hex(clientPublicKey))
+            print('client private key:', hex(cilentPrivateKey))
+            msg = {'set':'initialParametrs','clientPublicKey':clientPublicKey}
+            message = json.dumps(msg).encode('utf-8')
+            s.sendall(message)
 
         if data.startswith('SERVER_PUBLIC_KEY'):                # Receiving servers's public key
             server_public = int(re.findall('[0-9]+' , data)[0])
